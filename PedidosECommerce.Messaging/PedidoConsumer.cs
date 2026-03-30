ffb9cb8d8d8661ab325a4aa36b9854b711deb586
Emersumm <emersonbk15@gmail.com>
2026-03-30 17:30:45 -0300

    fix: classe consumer restaurada

--- PedidosECommerce.Messaging/PedidoConsumer.cs
+++ PedidosECommerce.Messaging/PedidoConsumer.cs
@@ -1,7 +1,23 @@
-﻿namespace PedidosECommerce.Messaging
-{
-    public class Class1
-    {
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using PedidosECommerce.Application.Services;
+using PedidosECommerce.Domain;
 
+namespace PedidosECommerce.Messaging
+{
+    public class PedidoConsumer : IConsumer<PedidoCriadoEvent>
+    {
+        private readonly IPedidoService _pedidoService;
+        private readonly ILogger<PedidoConsumer> _logger;
+        public PedidoConsumer(IPedidoService pedidoService, ILogger<PedidoConsumer> logger)
+        {
+            _pedidoService = pedidoService;
+            _logger = logger;
+        }
+        public async Task Consume(ConsumeContext<PedidoCriadoEvent> context)
+        {
+            _logger.LogInformation("Consumindo mensagem: {CorrelationId}", context.CorrelationId);
+            await _pedidoService.ProcessarPedido(context.Message.Id,context.CorrelationId ?? Guid.NewGuid());
+        }
     }
 }
